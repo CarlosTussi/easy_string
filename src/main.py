a0daf1ca0cdc02072cb0844887f55ab4ce92d56e
Carlos Eduardo Tussi <38245497+CarlosTussi@users.noreply.github.com>
2025-03-10 11:34:27 +0100

    Removing moved files

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-# Always run from easy_string/test
-
-from easy_string import *
-
-if __name__ == '__main__':
-    print(word_count("hello world hello"))
-    print(word_count("hello world bye world hello bye world"))
